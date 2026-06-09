45593489d90f98d2281ede2bb6a4937097978771
LucasPC-hub <lucas.pc99@hotmail.com>
2026-06-09 20:13:05 -0300

    chore: nix dev shell (flake/direnv) + ignore .direnv

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "cockpit-s3 dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ nodejs_22 pnpm ];
+      };
+    };
+}
